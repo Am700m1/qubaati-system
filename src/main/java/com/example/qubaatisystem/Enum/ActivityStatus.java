ec2a968a9c9b615797fd60ca8ea282bb6ca1cc86
Ammar <s202024140@kfupm.edu.sa>
2026-06-16 16:35:24 +0300

    initial Enum for some entity fields

---
+++ src/main/java/com/example/qubaatisystem/Enum/ActivityStatus.java
@@ -0,0 +1,13 @@
+package com.example.qubaatisystem.Enum;
+
+public enum ActivityStatus {
+    DRAFT,
+    PENDING_REVIEW,
+    APPROVED,
+    REJECTED,
+    ASSIGNED,
+    IN_PROGRESS,
+    SUBMITTED,
+    COMPLETED,
+    ARCHIVED
+}
---
+++ src/main/java/com/example/qubaatisystem/Enum/UserRole.java
@@ -0,0 +1,7 @@
+package com.example.qubaatisystem.Enum;
+
+public enum UserRole {
+    STUDENT,
+    TEACHER,
+    ADMIN
+}
